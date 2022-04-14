cdaca3eccacfcea4dd9f59b5345f746913745694
SlavenDj <53652906+SlavenDj@users.noreply.github.com>
2022-04-14 11:51:21 +0200

    query format

--- imports/querys.php
+++ imports/querys.php
@@ -1,21 +1,26 @@
 <?php
 
 //svi predmeti
-$sviPred = "SELECT id, naziv FROM predmeti";
+$sviPred = 
+"SELECT id, naziv 
+FROM predmeti";
 
 function sviPred($x)
 {
-    return "SELECT veza_razred_predmet.id, naziv
-            FROM   `veza_razred_predmet`
-            INNER JOIN `predmeti`
-                    ON predmeti.id = veza_razred_predmet.predmet
-                    AND razred = $x 
-            ORDER  BY redni_broj";
+    return 
+    "SELECT veza_razred_predmet.id, naziv
+    FROM   `veza_razred_predmet`
+    INNER JOIN `predmeti`
+            ON predmeti.id = veza_razred_predmet.predmet
+            AND razred = $x 
+    ORDER  BY redni_broj";
 }
 
 function qryDelete($x)
 {
-    return "DELETE FROM `veza_razred_predmet` 
+    return 
+    "DELETE 
+    FROM `veza_razred_predmet` 
     WHERE ID=" . (int)$_POST["izbrisi_ovaj_predmet_$x"];
 }
 function qryInsert($x)
