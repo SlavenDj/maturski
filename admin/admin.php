247d44b7869fa2ff4402db664767c536783442fe
Slaven Đervida <slavendjervida@gmail.com>
2022-06-20 22:20:11 +0200

    moving table and checking in funs.php

--- admin/admin.php
+++ admin/admin.php
@@ -5,22 +5,7 @@ include '../admin_files/conn.php';
 include '../admin_files/querys.php';
 include "../admin_files/add_erase.php";
 
-// * Log Out
-if (isset($_GET["logout"]) || !isset($_SESSION["username"]))
-    logOut();
-
-if (isset($_POST["new-order-nuber"]))
-    $mydb->query($_POST["new-order-nuber"]);
-
-if (isset($_POST["new-admin-username"]) && isset($_POST["new-admin-password"]))
-    addNewUser($mydb, $_POST["new-admin-username"], $_POST["new-admin-password"]);
-
-if (isset($_POST["new-pwrd-1"]))
-    $mydb->query(updatePasswordQuery($_SESSION["userID"], $_POST["new-pwrd-1"]));
-
-if (isset($_POST["new-username"]))
-    $mydb->query("UPDATE `admin` set `username`='" . $_GET["new-username"] . "'");
-?>
+adminChechingVariables($mydb);?>
 <!DOCTYPE html>
 <html lang='sr'>
 
@@ -76,98 +61,7 @@ if (isset($_POST["new-username"]))
 
     </nav>
 
-    <?php
-    $idijeviPredmetaZaRacunanje = array(1, 2, 14, 20, 16);
-    echo '<table class="export">
-    <thead>
-        <tr>
-            <td rowspan="3">Rd. br.</td>
-            <td rowspan="3">Prezime i ime</td>
-            <td rowspan="3">Ime oca</td>
-            <td colspan="5" rowspan="2">Bodovi po uspjehu</td>
-            <td colspan="11">Bodovi po predmetima</td>
-            <td rowspan="3">Svega</td>
-        </tr>
-        <tr>
-            <td colspan="2">Srpski</td>
-            <td colspan="2">Strani j.</td>
-            <td colspan="2">Matem.</td>
-            <td colspan="2">Fizika</td>
-            <td colspan="2">Inform.</td>
-            <td rowspan="2">Ukupno</td>
-        </tr>
-        <tr>
-            <td>VI</td>
-            <td>VII</td>
-            <td>VIII</td>
-            <td>IX</td>
-            <td>Ukupno</td>
-            <td>VIII</td>
-            <td>IX</td>
-            <td>VIII</td>
-            <td>IX</td>
-            <td>VIII</td>
-            <td>IX</td>
-            <td>VIII</td>
-            <td>IX</td>
-            <td>VIII</td>
-            <td>IX</td>
-        </tr>
-    </thead>';
-    $pregledaniUcenici = $mydb->query("SELECT * FROM ucenik WHERE pregledano=1");
-    $redniBroj = 0;
-    while ($row = $pregledaniUcenici->fetch_array()) {
-        $redniBroj++;
-        echo "<tr>
-                <td>
-                    $redniBroj. 
-                </td>
-                <td>
-                    {$row["prezime"]} {$row["ime"]} 
-                </td>
-                <td>
-                    {$row["ime_oca"]} 
-                </td>";
-        $opstiUpseh = 0;
-        $predmeti = 0;
-        for ($razred = 6; $razred <= 9; $i++) {
-            $ocene = $mydb->query("SELECT * FROM ocena WHERE ucenik={$row["ID"]} AND razred=$i");
-            $prosek = 0;
-            $brojPredmeta = 0;
-            while ($jednaOcena = $ocene->fetch_array()) {
-                $prosek += $jednaOcena["ocena"];
-                $brojPredmeta++;
-                if (in_array($jednaOcena["predmet"], $idijeviPredmetaZaRacunanje) && $razred >= 8)
-                    $predmeti += $jednaOcena["ocena"] / 5.0 * 2;
-            }
-            if ($brojPredmeta) {
-                echo "<td>" . round($prosek / $brojPredmeta, 2) . "</td>";
-
-                $opstiUpseh += ($razred <= 7) ? $prosek / $brojPredmeta  * 2 : $prosek / $brojPredmeta  * 3;
-            }
-        }
-        echo " <td>" . round($opstiUpseh, 2) . "</td>";
-        echo "<td>" . oceneIz($mydb, 14, 8, $row["ID"]) . "</td>"; //srpski 
-        echo "<td>" . oceneIz($mydb, 14, 9, $row["ID"]) . "</td>";
-
-        echo "<td>" . oceneIz($mydb, 16, 8, $row["ID"]) . "</td>"; //eng 
-        echo "<td>" . oceneIz($mydb, 16, 9, $row["ID"]) . "</td>";
-
-        echo "<td>" . oceneIz($mydb, 1, 8, $row["ID"]) . "</td>"; //matematika 
-        echo "<td>" . oceneIz($mydb, 1, 9, $row["ID"]) . "</td>";
-
-        echo "<td>" . oceneIz($mydb, 2, 8, $row["ID"]) . "</td>"; //fizika 
-        echo "<td>" . oceneIz($mydb, 2, 9, $row["ID"]) . "</td>";
-
-        echo "<td>" . oceneIz($mydb, 20, 8, $row["ID"]) . "</td>"; //informatika 
-        echo "<td>" . oceneIz($mydb, 20, 9, $row["ID"]) . "</td>";
-
-        echo " <td>" . round($predmeti, 2) . "</td>";
-        echo " <td>" . round($predmeti + $opstiUpseh, 2) . "</td>";
-    }
-
-    echo "</table> ";
-    ?>
+    <?php prikaziTabeluPregledanihUcenika($mydb) ?>
     <form action="export.php">
         <button>Export in Excel file</button>
     </form>
